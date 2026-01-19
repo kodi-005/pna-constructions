f32e1e697fb65c706cc7979f5898512e7e410647
kodi-005 <kodithuwakkuomal8@gmail.com>
2026-01-19 21:03:38 +0530

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -707,7 +707,7 @@ export default function About() {
           <div className="text-center">
             <div className="flex justify-center mb-4">
               <img 
-                src="/logowhite.png" 
+                src="/pnalogo.png" 
                 alt="PNA Construction" 
                 className="h-16 md:h-20 w-auto object-contain"
               />
